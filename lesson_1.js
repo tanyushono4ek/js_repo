1f1a60ab82b0a48c76e9ed5d7babd78e47a18ea2
tanyushono4ek <ten87@mail.ru>
2021-09-17 15:16:20 +0300

    First commit

---
+++ lesson_1.js
@@ -0,0 +1,60 @@
+< !DOCTYPE html >
+
+    <
+    head >
+    <
+    meta charset = "UTF-8" >
+    <
+    /meta>
+
+
+
+
+
+
+//Выразить температуру в фаренгейтах
+
+<
+script >
+    var celc
+var far
+celc = prompt("Hello! Enter temperature in Celcius")
+far = (9 / 5) * celc + 32
+alert("Temperature in Farenheit is " + far) <
+    /script >
+
+
+
+// При вводе админа выводится имя Василий
+<
+script >
+    var admin
+var name = "Vasiliy"
+admin = name
+alert("Variable ADMIN equals " + admin) <
+    /script>
+
+
+//При вводе 1000 + "108" на экран выводится 1000108, потому что "108" распознается как текст
+<
+script >
+    alert(1000 + "108") <
+    /script>
+
+
+// Этого задания я не нашла в методичке и в заданиях, но решила повторить после разбора на уроке
+<
+script >
+    var a = +prompt("Enter a")
+var b = +prompt("Enter b")
+b = a - b
+a = a - b
+b = b = a
+alert("a =" + a + "\nb =" = b)
+
+
+<
+/script>
+
+<
+/head >
